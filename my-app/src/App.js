ca94a2bf90499466493d308b17b01df8fe98d86a
Simone Roy <simonecarolynr@gmail.com>
2020-04-13 17:18:18 -0600

    stretch started

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import UserCard from './UserCard';
+import FollowerCard from './FollowerCard';
 
 class App extends React.Component {
   constructor() {
@@ -11,6 +12,8 @@ class App extends React.Component {
       name: '',
       followers: '',
       following: '',
+      follower: '',
+      followerPic: '',
     }
   }
 
@@ -19,7 +22,7 @@ class App extends React.Component {
     .get('https://api.github.com/users/simonecarolynr')
     .then(response => {
       let user = response.data; 
-      console.log(user)
+      console.log(response)
 
       this.setState({
         profilePic: user.avatar_url,
@@ -31,6 +34,21 @@ class App extends React.Component {
     .catch(error => {
       console.log(error)
     });
+
+    axios
+    .get('https://api.github.com/users/simonecarolynr/followers')
+    .then(response => {
+      let followers = response.data;
+      console.log(followers)
+
+      this.setState({
+        follower: followers.login,
+        followerPic: followers.imgSrc,
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 
 
     }
@@ -44,6 +62,13 @@ class App extends React.Component {
           followers={this.state.followers} 
           following={this.state.following} 
         />
+        {this.followers.map(follower => {
+          return <FollowerCard 
+            name={this.state.follower} 
+            imgSrc={this.state.followerPic} 
+          />
+        }
+        )}
       </div>
     );
   }
--- my-app/src/FollowerCard.js
+++ my-app/src/FollowerCard.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-export default class UserCard extends React.Component {
+export default class FollowerCard extends React.Component {
     constructor(props) {
         super();
     }
     render() {
-        let { name, imgSrc, followers, following } = this.props;
+        let { name, imgSrc } = this.props;
         return ( 
             <div className='card'>
                 <img className='img' src={imgSrc} />
                 <h3>{name}</h3>
-                <h3>{followers}</h3>
-                <h3>{following}</h3>
             </div>
         )
     }
